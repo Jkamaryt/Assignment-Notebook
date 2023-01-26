5bd4e61133016b084af527006bb403d640081f6f
Jack Kamaryt <jkamaryt46@gmail.com>
2023-01-26 09:49:48 -0600

    Edited assignment view

--- Assignment Notebook/ContentView.swift
+++ Assignment Notebook/ContentView.swift
@@ -2,20 +2,38 @@
 //  ContentView.swift
 //  Assignment Notebook
 //
-//  Created by Jack Kamaryt on 1/25/23.
+//  Created by Jack Kamaryt & Quinn Kozak on 1/25/23.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assignmentItems = [
+    Assignments(course: "Math", description: "Homework", dueDate: Date()),
+    Assignments(course: "English", description: "Homework", dueDate: Date()),
+    Assignments(course: "Science", description: "Test", dueDate: Date())
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+       NavigationView {
+           List {
+               ForEach(assignmentItems) { item in
+                   VStack(alignment: .leading) {
+                       Text(item.course)
+                           .font(.headline)
+                       Text(item.description)
+                   }
+               }
+               .onMove { indices, newOffset in
+                   assignmentItems.move(fromOffsets: indices, toOffset: newOffset)
+               }
+               .onDelete { indexSet in
+                   assignmentItems.remove(atOffsets: indexSet)
+               }
+           }
+           .navigationBarTitle("Assignments", displayMode: .inline)
+           .navigationBarItems(leading: EditButton())
+       }
     }
 }
 
@@ -23,4 +41,11 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Assignments: Identifiable, Codable{
+    var id = UUID()
+    var course = String()
+    var description = String()
+    var dueDate = Date()
 }
