ffd896f8fb215f86b6913057ed7b62bd7e755a08
Jack Kamaryt <jkamaryt46@gmail.com>
2023-01-30 10:21:13 -0600

    Enabled data persistence

--- Assignment Notebook/AssignmentList.swift
+++ Assignment Notebook/AssignmentList.swift
@@ -6,10 +6,23 @@
 //
 
 import Foundation
-class AssignmentList: ObservableObject{
-    @Published var items = [AssignmentItem(course: "Math", description: "3.1 Notes", dueDate: Date(), time: "00:00" ) ,
-                            AssignmentItem(course: "Enlgish", description: "Essay Outline", dueDate: Date(), time: "00:00" )
-                             ,
-                             AssignmentItem(course: "Science", description: "Brain Lab", dueDate: Date(), time: "00:00" )
-                             ]
-                             }
+class AssignmentList: ObservableObject {
+    @Published var items : [AssignmentItem] {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "data")
+            }
+        }
+    }
+    init() {
+        if let items = UserDefaults.standard.data(forKey: "data") {
+            let decoder = JSONDecoder()
+            if let decoded = try? decoder.decode([AssignmentItem].self, from: items) {
+                self.items = decoded
+                return
+            }
+        }
+        items = []
+    }
+}
