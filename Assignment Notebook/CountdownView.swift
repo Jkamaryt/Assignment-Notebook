7643089e54b2b2476f2f0d541e7852475a47bebf
Jack Kamaryt <jkamaryt46@gmail.com>
2023-02-02 14:15:06 -0600

    Added colors, rounded edges, and countdown works as far as days

--- Assignment Notebook/CountdownView.swift
+++ Assignment Notebook/CountdownView.swift
@@ -9,52 +9,43 @@ import SwiftUI
 
 struct CountdownView: View {
     
-    @State private var timeRemaining = 100
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @Environment(\.scenePhase) var scenePhase
-    @State private var isActive = true
-    
+   
     var body: some View {
         NavigationView {
-            ZStack {
-                Image("background")
-                    .resizable()
-                    .ignoresSafeArea()
+            VStack {
                 
-                VStack {
-                    Text("Time: \(timeRemaining)")
-                        .font(.largeTitle)
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 20)
-                        .padding(.vertical, 5)
-                        .background(.black.opacity(0.75))
-                        .clipShape(Capsule())
+                Text("\(getNumberOfDaysUntilEndOfSemester()) Days till summer!")
+                    .frame(width: 350, height: 50, alignment: .center)
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(Capsule())
                     
-                    ZStack {
-                        
-                    }
-                    .onReceive(timer) {time in
-                        guard isActive else {return}
-                        
-                        if timeRemaining > 0
-                        {
-                            timeRemaining -= 1
-                        }
-                    }
-                    .onChange(of: scenePhase) { newPhase in
-                        if newPhase == .active {
-                            isActive = true
-                        } else {
-                            isActive = false
-                        }
-                    }
-                    
-                }
+                
             }
+            
         }
-        
     }
 }
+
+func getNumberOfDaysUntilEndOfSemester() -> Int
+        {
+            
+            
+            let finalSemesterDateDateComponents = DateComponents(month: 6, day: 1, hour: 23, minute: 59, second: 59) //May 31st date +1 counting today
+            
+            let currentDate = Date() //today -> day 246...and increasing...
+            
+            if let nextFinalSemesterDate = Calendar.current.nextDate(after: currentDate, matching: finalSemesterDateDateComponents, matchingPolicy: .strict),
+               let daysUntilNextFinalSemesterDate = Calendar.current.dateComponents([.day], from: currentDate, to: nextFinalSemesterDate).day {
+                return daysUntilNextFinalSemesterDate
+            }
+            
+            return 0
+        }
+
 
 struct CountdownView_Previews: PreviewProvider {
     static var previews: some View {
